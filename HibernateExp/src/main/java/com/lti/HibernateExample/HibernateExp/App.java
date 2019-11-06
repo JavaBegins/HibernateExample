5a0dd86f6dd80cdb68fdd892b0a8e4f44d925c86
anki <beginsjava144@gmail.com>
2019-11-06 15:11:29 +0530

    AnkiE

---
+++ HibernateExp/src/main/java/com/lti/HibernateExample/HibernateExp/App.java
@@ -0,0 +1,13 @@
+package com.lti.HibernateExample.HibernateExp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ HibernateExp/src/main/java/com/lti/HibernateExample/HibernateExp/Employee.java
@@ -0,0 +1,28 @@
+package com.lti.HibernateExample.HibernateExp;
+
+public class Employee 
+{
+private int id;
+private String empname;
+private String branch;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getEmpname() {
+	return empname;
+}
+public void setEmpname(String empname) {
+	this.empname = empname;
+}
+public String getBranch() {
+	return branch;
+}
+public void setBranch(String branch) {
+	this.branch = branch;
+}
+
+
+}
